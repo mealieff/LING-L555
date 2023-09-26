13048db9b8513db9d4a2c4b821fe8b4284ed9fc0
Melissa Lieffers <melissalieffers@Melissas-MacBook-Air-4.local>
2023-09-26 15:30:01 -0400

    here again

--- tokeniser.py
+++ tokeniser.py
@@ -3,10 +3,25 @@ import sys
 from signal import signal, SIGPIPE, SIG_DFL
 signal(SIGPIPE,SIG_DFL) 
 
-punctuation = [',', '.', '?', '\'', ':', ';', '...', "'", '"', '(', '('] 
+counter = 0
+punctuation = [',', '.', '?', '\'', ':', ';', '...', "'", '"', '(', '(']
 
-for s in sys.stdin.readlines():
+for ln in sys.stdin.readlines():
+	if ln.strip () == '':
+		continue
+	counter = counter + 1
+		# Print the sentence ID number
+	print('#sent_ID =', counter) 
+		# Print the sentence from the segmenter
+	print('#text =', ln,end="")
+		# Print the numbered tokens with ten columns with white space accounted for with a tab
+	if ln.strip() == '':
+		continue
+	s = ln
 	for p in punctuation:	
-		s = s.replace(f'{p}', f' {p} ')
-	s_replacespace = s.replace(' ', '\n')
-	print(s_replacespace)
+		s = s.replace(f'{p}',f' {p} ')
+	t = s.split(' ')
+	for i,n in enumerate(t):
+		if n.strip() == '':
+			continue
+		print('%d\t %s\t_\t_\t_\t_\t_\t_\t_\t_\t_'%(i+1,n))
